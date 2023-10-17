40af7e6abf97c3dd3ecbb2da92e9770fd6875dd4
Realwale <olawaleagboola41@gmail.com>
2023-10-17 22:12:10 +0100

    implement controllers

---
+++ src/main/java/org/vfdtech/alm/controller/DebtMaturityProfileController.java
@@ -0,0 +1,40 @@
+package org.vfdtech.alm.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.vfdtech.alm.dto.DebtMaturityResponse;
+import org.vfdtech.alm.service.DebtMaturityProfileService;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/compute/debt-maturity")
+public class DebtMaturityProfileController {
+
+    private final DebtMaturityProfileService debtMaturityProfileService;
+
+    @GetMapping
+    public ResponseEntity<DebtMaturityResponse> computeDebtMaturity(
+            @RequestParam(required = false) Long days,
+            @RequestParam(required = false) Long fromDay,
+            @RequestParam(required = false) Long toDay) {
+
+        String bucket;
+
+        if (days != null) {
+            bucket = days.toString();
+        } else if (fromDay != null && toDay != null) {
+            bucket = fromDay + "-" + toDay;
+        } else {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        DebtMaturityResponse response = debtMaturityProfileService.computeAndSaveDebtMaturityProfileForBucket(bucket);
+        return ResponseEntity.ok(response);
+    }
+
+}
---
+++ src/main/java/org/vfdtech/alm/controller/LiabilityReportController.java
@@ -0,0 +1,51 @@
+package org.vfdtech.alm.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.vfdtech.alm.model.LiabilityReport;
+import org.vfdtech.alm.service.LiabilityReportService;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/liability-reports")
+public class LiabilityReportController {
+
+    private final LiabilityReportService liabilityReportService;
+
+    @PostMapping
+    public ResponseEntity<LiabilityReport> createLiabilityReport(@RequestBody LiabilityReport liabilityReport) {
+        return new ResponseEntity<>(liabilityReportService.saveOrUpdateLiabilityReport(liabilityReport), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LiabilityReport>> getAllLiabilityReports() {
+        return new ResponseEntity<>(liabilityReportService.getAllLiabilityReports(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LiabilityReport> getLiabilityReportById(@PathVariable String id) {
+        LiabilityReport report = liabilityReportService.getLiabilityReportById(id);
+        if(report != null) {
+            return new ResponseEntity<>(report, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LiabilityReport> updateLiabilityReport(@PathVariable String id, @RequestBody LiabilityReport liabilityReport) {
+        return new ResponseEntity<>(liabilityReportService.saveOrUpdateLiabilityReport(liabilityReport), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLiabilityReport(@PathVariable String id) {
+        liabilityReportService.deleteLiabilityReportById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
