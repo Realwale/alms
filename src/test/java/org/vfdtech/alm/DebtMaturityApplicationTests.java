4e7c2e170166a400ffefc8ff4131863054067115
Realwale <olawaleagboola41@gmail.com>
2023-10-17 22:08:56 +0100

    initialize project

---
+++ src/test/java/org/vfdtech/alm/DebtMaturityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vfdtech.alm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtMaturityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
