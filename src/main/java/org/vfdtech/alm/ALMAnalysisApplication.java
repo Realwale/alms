560133b3e2f4f38d2046c737189ef21fff36b82c
Realwale <olawaleagboola41@gmail.com>
2023-10-17 22:08:02 +0100

    initialize project

---
+++ src/main/java/org/vfdtech/alm/ALMAnalysisApplication.java
@@ -0,0 +1,13 @@
+package org.vfdtech.alm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALMAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ALMAnalysisApplication.class, args);
+    }
+
+}
