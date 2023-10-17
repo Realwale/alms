d75c38777462e8c290a0e8af2df3a2944dbdebbb
Realwale <olawaleagboola41@gmail.com>
2023-10-17 22:12:25 +0100

    add swagger

---
+++ src/main/java/org/vfdtech/alm/config/OpenApiSwagger.java
@@ -0,0 +1,21 @@
+package org.vfdtech.alm.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                description = "Open API documentation for ALM Web Application API",
+                title = "ALM Web Application",
+                version = "1.0",
+                license = @License(name = "Apache License", url = "https://www.apache.org/licenses/LICENSE-2"),
+                termsOfService = "Terms of Service"
+        )
+
+)
+public class OpenApiSwagger {
+
+
+}
