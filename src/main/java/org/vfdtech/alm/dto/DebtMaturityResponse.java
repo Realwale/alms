d859f258510a35a2ef56eab6cc3c11c94383a52b
Realwale <olawaleagboola41@gmail.com>
2023-10-17 22:11:45 +0100

    add DebtMaturityResponse

---
+++ src/main/java/org/vfdtech/alm/dto/DebtMaturityResponse.java
@@ -0,0 +1,18 @@
+package org.vfdtech.alm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DebtMaturityResponse {
+
+    private BigDecimal result;
+
+}
